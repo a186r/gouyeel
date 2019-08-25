50131169d026dc5df4788e60f693392357d81a61
a186r <hi.aewalle@gmail.com>
2019-08-25 14:38:38 +0800

    转移eth

---
+++ main/transfer_eth.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"context"
+	"crypto/ecdsa"
+	"fmt"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"log"
+	"math/big"
+)
+
+func main() {
+//	连接到客户端
+	client, err := ethclient.Dial("http://localhost:8545")
+	if err != nil{
+		log.Fatal(err)
+	}
+
+//	导入私钥
+	privateKey, err := crypto.HexToECDSA("4b3083dec275a833bdec039205cc4af69607cd5eca1a52e8423bc5a4d3c8eb2f")
+	if err != nil{
+		log.Fatal(err)
+	}
+//	想要获取nonce, 需要调用一个方法, 该方法需要我们的address, 所以从私钥派生address出来
+	publicKey := privateKey.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok{
+		log.Fatal("密钥格式不对")
+	}
+	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
+
+//	然后获取我们需要的随机数
+	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
+	if err != nil{
+		log.Fatal(err)
+	}
+
+	value := big.NewInt(1 * 1E18)
+	gasLimit := uint64(21000)
+	gasPrice, err := client.SuggestGasPrice(context.Background())
+	if err != nil{
+		log.Fatal(err)
+	}
+	toAddress := common.HexToAddress("0x2D022555f3A916761141db09622d93F2459B1DaC")
+//	数据data
+	var data []byte
+//	组装交易数据
+	tx := types.NewTransaction(nonce, toAddress, value, gasLimit, gasPrice, data)
+
+//  下一步是使用发件人的私钥对事务进行签名。
+//  为此，我们调用SignTx方法，该方法接受一个未签名的事务和我们之前构造的私钥。
+//  SignTx方法需要EIP155签名者，这个也需要我们先从客户端拿到链ID。
+	chainId, err := client.NetworkID(context.Background())
+	if err != nil{
+		log.Fatal(err)
+	}
+
+	signedTx, err := types.SignTx(tx, types.NewEIP155Signer(chainId), privateKey)
+	if err != nil{
+		log.Fatal(err)
+	}
+	err = client.SendTransaction(context.Background(), signedTx)
+	if err != nil{
+		log.Fatal(err)
+	}
+
+	fmt.Printf("tx sent: %s", signedTx.Hash().Hex())
+}
