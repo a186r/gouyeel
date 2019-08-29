9be670086b5929a8b6a35e3c1200697e634827b7
a186r.home.iMac <hi.aewalle@gmail.com>
2019-08-29 08:08:15 +0800

    读取0x Protocol的事件日志

---
+++ contracts/exchange/Exchange.sol
@@ -0,0 +1,30 @@
+pragma solidity 0.5.11;
+
+contract Exchange {
+    event LogFill(
+        address indexed maker,
+        address taker,
+        address indexed feeRecipient,
+        address makerToken,
+        address takerToken,
+        uint filledMakerTokenAmount,
+        uint filledTakerTokenAmount,
+        uint paidMakerFee,
+        uint paidTakerFee,
+        bytes32 indexed tokens, // keccak256(makerToken, takerToken), allows subscribing to a token pair
+        bytes32 orderHash
+    );
+
+    event LogCancel(
+        address indexed maker,
+        address indexed feeRecipient,
+        address makerToken,
+        address takerToken,
+        uint cancelledMakerTokenAmount,
+        uint cancelledTakerTokenAmount,
+        bytes32 indexed tokens,
+        bytes32 orderHash
+    );
+
+    event LogError(uint8 indexed errorId, bytes32 indexed orderHash);
+}
